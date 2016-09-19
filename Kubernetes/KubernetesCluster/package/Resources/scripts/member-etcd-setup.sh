91640f3e7fa0840f3cc85e629ff559fedc191a8b
Artem Silenkov <asilenkov@mirantis.com>
2016-09-19 17:44:33 +0300

    [app][calico][kubernetes] Enable calico networking in k8s application
      - binaries and scripts updated to use calico networking
    
    Change-Id: I3187b9cc171aa15caa4912a665c642ab057b4eae

--- Kubernetes/KubernetesCluster/package/Resources/scripts/member-etcd-setup.sh
+++ Kubernetes/KubernetesCluster/package/Resources/scripts/member-etcd-setup.sh
@@ -4,6 +4,26 @@
 # $2 - IP
 # $3 - ETCD_INITIAL_CLUSTER
 
+# Install Calico on worker
+mkdir -p /opt/cni/bin
+cp /opt/copy/cni/bin/* /opt/cni/bin
+ln -s /opt/bin/calicoctl /usr/bin/calicoctl
+docker load < /opt/copy/calico-node.tar
+
+sed -i.bak "s/%%MASTER_IP%%/$2/g" /opt/copy/network-environment
+sed -i.bak "s/%%IP%%/$2/g" /opt/copy/network-environment
+cp -f /opt/copy/network-environment /etc
+
+sed -i.bak "s/%%IP%%/$2/g" systemd/calico-node.service
+cp -f systemd/calico-node.service /etc/systemd/system/
+systemctl enable calico-node.service
+
+mkdir -p /etc/cni/net.d
+sed -i.bak "s/%%MASTER_IP%%/$2/g" /opt/copy/10-calico.conf
+cp -f /opt/copy/10-calico.conf /etc/cni/net.d
+
+systemctl start calico-node
+
 mkdir /var/lib/etcd
 
 if [[ $(which systemctl) ]]; then
